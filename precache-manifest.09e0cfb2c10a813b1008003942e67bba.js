dba35691511d7aa05c96fef867acb777ed465554
pmnb34 <pmnb34@gmail.com>
2019-08-08 21:43:01 +0900

    Updates

---
+++ precache-manifest.09e0cfb2c10a813b1008003942e67bba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2ad9d12ccb04713299499229453e5b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dd720378edc42c6248b6",
+    "url": "/my-app/static/css/main.68ddb44e.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/my-app/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "dd720378edc42c6248b6",
+    "url": "/my-app/static/js/main.62be4055.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
